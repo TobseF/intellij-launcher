c8d3808cb0c3e6f14b0596382bdcb9fd0f07a8a7
Tobse <Tobse4Git@gmail.com>
2018-04-04 00:59:08 +0200

    Add initial RunIntelliJ kotlin script

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # intellij-launcher
 Kotlin script that launches the latest installed IntelliJ with a specified project
+
+## Usage
+
+**Variant 1**
+Rename script to `.kts` and run as kotlin script.
+
+**Variant 2**
+Run wih  [blaze](https://github.com/fizzed/blaze):
+``` shell
+java -jar blaze.jar runIntelliJ
+
+# or with project
+java -jar -Dproject=C:\Users\your-project blaze.jar runIntelliJ
+```
---
+++ src/RunIntelliJ.kt
@@ -0,0 +1,51 @@
+import java.io.File
+import java.nio.file.Path
+import java.nio.file.Paths
+
+
+val appData = System.getenv("LOCALAPPDATA")!!
+var toolboxPath = "$appData/JetBrains/Toolbox"
+var channelsPath = "$toolboxPath/apps/IDEA-U/ch-0/"
+var runtimeSuffix = "/bin/idea64.exe"
+
+fun isVersion(version: String) = version.matches("[0-9]+(\\.[0-9]+)*".toRegex())
+
+fun runIntelliJ() {
+    File(channelsPath).walkTopDown().maxDepth(1).map { it.name }.filter { isVersion(it) }.map { Version(it) }
+            .sortedDescending().first().let { getPath(it) }.let {
+                log(it)
+                exec(it)
+            }
+}
+
+fun log(path: Path) = println("executing $path with project ${getProjectArg() ?: "- none"}")
+
+fun getProjectArg(): String? = System.getProperty("project")
+
+fun exec(path: Path) = Runtime.getRuntime().exec("$path ${getProjectArg().orEmpty()}")
+
+fun getPath(version: Version) = Paths.get(channelsPath + version.version + runtimeSuffix).toAbsolutePath()!!
+
+fun main(args: Array<String>) {
+    runIntelliJ()
+}
+
+data class Version(val version: String) : Comparable<Version> {
+
+    private fun String.split() = this.split("\\.".toRegex()).toTypedArray()
+
+    override fun compareTo(other: Version): Int {
+        val thisParts = version.split()
+        val thatParts = other.version.split()
+        val length = Math.max(thisParts.size, thatParts.size)
+        for (i in 0 until length) {
+            val thisPart = if (i < thisParts.size) Integer.parseInt(thisParts[i])
+            else 0
+            val thatPart = if (i < thatParts.size) Integer.parseInt(thatParts[i])
+            else 0
+            if (thisPart < thatPart) return -1
+            if (thisPart > thatPart) return 1
+        }
+        return 0
+    }
+}
